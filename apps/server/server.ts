482a0de2c6b852c1e1a624331d98fd2dc9cb2b33
Nataliya Soina <natashaford@mail.ru>
2026-01-09 18:37:12 +0300

    initial commit, add some infra

---
+++ apps/server/server.ts
@@ -0,0 +1,23 @@
+import { createServer } from 'http'
+import { createSchema, createYoga } from 'graphql-yoga'
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'Hello from Yoga!'
+      }
+    }
+  })
+})
+
+const server = createServer(yoga)
+
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
