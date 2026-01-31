405b410184cbc697cbbf076b4a95a46d53e059b6
Nataliya Soina <natashaford@mail.ru>
2026-01-31 18:35:48 +0300

    add tailwind

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,7 +4,8 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'url'
-import { nitro } from 'nitro/vite'
+
+import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: {
@@ -14,12 +15,11 @@ const config = defineConfig({
   },
   plugins: [
     devtools(),
-    nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-
+    tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
